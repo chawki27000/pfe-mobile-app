336415543da298d219a065fae25359eef126f61f
chawki benchehida <chawki27000@hotmail.com>
2017-05-24 20:25:44 +0100

    add mor fragments and stable activity

--- app/src/main/java/com/dev/chawki/alpha/ResultFragment.java
+++ app/src/main/java/com/dev/chawki/alpha/ResultFragment.java
@@ -3,14 +3,16 @@ package com.dev.chawki.alpha;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class ResultFragment extends Fragment {
 
+    Button buttonResult;
 
     public ResultFragment() {
         // Required empty public constructor
@@ -21,7 +23,21 @@ public class ResultFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false);
+        View view = inflater.inflate(R.layout.fragment_result, container, false);
+
+        buttonResult = (Button) view.findViewById(R.id.button_result);
+
+        buttonResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Begin the transaction
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.fragment_placeholder, new InstableFragment());
+                ft.commit();
+            }
+        });
+
+        return view;
     }
 
 
---
+++ app/src/main/java/com/dev/chawki/alpha/StableActivity.java
@@ -0,0 +1,13 @@
+package com.dev.chawki.alpha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stable);
+    }
+}
