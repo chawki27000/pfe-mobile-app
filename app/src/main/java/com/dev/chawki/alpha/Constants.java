240d139d163aa15716eea1cb9dd9cdd7e165edd2
chawki benchehida <chawki27000@hotmail.com>
2017-05-27 01:09:46 +0100

    refactor login activity

---
+++ app/src/main/java/com/dev/chawki/alpha/Constants.java
@@ -0,0 +1,16 @@
+package com.dev.chawki.alpha;
+
+/**
+ * Created by chawki on 26/05/17.
+ */
+public class Constants {
+
+    // Constants
+    public final static String URL = "http://6ae8b7bf.ngrok.io/";
+    public final static String TAG = "DEV";
+
+    // Global state
+    public static String user_id = "";
+    public static String token = "";
+
+}
--- app/src/main/java/com/dev/chawki/alpha/LoginActivity.java
+++ app/src/main/java/com/dev/chawki/alpha/LoginActivity.java
@@ -1,16 +1,23 @@
 package com.dev.chawki.alpha;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
-import okhttp3.MultipartBody;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -23,6 +30,8 @@ public class LoginActivity extends AppCompatActivity {
     private Button button_connection;
     private EditText edittext_username;
     private EditText edittext_password;
+
+    private ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +51,89 @@ public class LoginActivity extends AppCompatActivity {
                 String username = edittext_username.getText().toString();
                 String password = edittext_password.getText().toString();
 
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                // AsyncRequest
+                AsyncRequest request = new AsyncRequest();
+                request.execute(Constants.URL, username, password);
+
+
+
             }
         });
+    }
+
+    private class AsyncRequest extends AsyncTask<String, Void, String> {
+
+        public final MediaType JSON
+                = MediaType.parse("application/x-www-form-urlencoded");
+
+
+
+
+
+        OkHttpClient client = new OkHttpClient();
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            // FormBuilder
+            RequestBody formBody = new FormBody.Builder()
+                    .add("username", params[1])
+                    .add("password", params[2])
+                    .build();
+
+            // Lunch request
+            Request request = new Request.Builder()
+                    .url(params[0]+"auth/login/")
+                    .post(formBody)
+                    .build();
+
+            try {
+                Response response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pd = ProgressDialog.show(LoginActivity.this, "Chargement", "Veuillez patienter...");
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            pd.dismiss();
+
+            // Parsing variable
+            boolean success;
+
+            // Parsing JSON
+            try {
+                JSONObject jsonObj = new JSONObject(s);
+
+                // parse
+                success = jsonObj.getBoolean("success");
+
+                if (success) {
+                    Constants.user_id = jsonObj.getString("id");
+                    Constants.token = jsonObj.getString("token");
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+
+                } else {
+
+                }
+
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
