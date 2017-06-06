ce001c0232a87a13198fd588bb611a93025fe3aa
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2017-06-06 07:40:05 +0100

    add splash screen

---
+++ app/src/main/java/com/dev/chawki/alpha/SplashActvity.java
@@ -0,0 +1,30 @@
+package com.dev.chawki.alpha;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_actvity);
+
+        // This solution will leak memory!  Don't use!!!
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }, 2000);
+
+    }
+
+}
